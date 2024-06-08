1ca429eb28f31fa5f68682833de232a26fdfa759
AdilsonJuniorBarboza <168775316+AdilsonJuniorBarboza@users.noreply.github.com>
2024-06-08 11:32:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const rankingData = [
         { posicao: 1, classe: 'Rogue', build: 'Heartseeker', nome: 'Veloso', tier: 110, tempo: '7:15', video: 'https://youtu.be/sAZwc2cc8t0?si=Efck8qAbZL-KYstU' },
         { posicao: 2, classe: 'Druída', build: 'Wind Shear', nome: 'Junin', tier: 105, tempo: '12:28', video: 'https://youtu.be/LFjy_89r1uc?si=pkB15t1EBXu0Ew1f' },
-        { posicao: 3, classe: 'Rogue', build: 'Heartseeker', nome: 'Serjão', tier: 100, tempo: '10:12', video: 'https://youtu.be/LFjy_89r1uc?si=pkB15t1EBXu0Ew1f' },
-        { posicao: 4, classe: 'Bárbaro', build: 'Bash', nome: 'Zézinho', tier: 97, tempo: '13:15', video: 'https://youtu.be/LFjy_89r1uc?si=pkB15t1EBXu0Ew1f' },
-        // Add more rows as needed
     ];
 
     const tbody = document.getElementById('ranking-body');
