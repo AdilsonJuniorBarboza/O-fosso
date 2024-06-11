2c065c0185a39531616c27724440d33808e289ef
AdilsonJuniorBarboza <168775316+AdilsonJuniorBarboza@users.noreply.github.com>
2024-06-10 21:13:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     const rankingData = [
         { posicao: 1, classe: 'Bárbaro', build: 'Bash/Bleed', nome: 'Shelby', tier: 116, tempo: '5:23', video: 'https://www.youtube.com/watch?v=-yQBAGswhyo' },
-        { posicao: 2, classe: 'Rogue', build: 'Heartseeker', nome: 'Junin', tier: 113, tempo: '14:19', video: 'https://youtu.be/kTVMPcm7IV4' },
-        { posicao: 3, classe: 'Rogue', build: 'Heartseeker', nome: 'Veloso', tier: 110, tempo: '7:15', video: 'https://youtu.be/sAZwc2cc8t0?si=Efck8qAbZL-KYstU' },
-        { posicao: 4, classe: 'Druída', build: 'Wind Shear', nome: 'Junin', tier: 105, tempo: '12:28', video: 'https://youtu.be/LFjy_89r1uc?si=pkB15t1EBXu0Ew1f' },
+        { posicao: 2, classe: 'Bárbaro', build: 'Thorns(Espinhos)', nome: 'Tanure', tier: 114, tempo: '9:57', video: 'https://youtu.be/NPQvHUZcEpI' },
+        { posicao: 3, classe: 'Rogue', build: 'Heartseeker', nome: 'Junin', tier: 113, tempo: '14:19', video: 'https://youtu.be/kTVMPcm7IV4' },
+        { posicao: 4, classe: 'Rogue', build: 'Heartseeker', nome: 'Veloso', tier: 110, tempo: '7:15', video: 'https://youtu.be/sAZwc2cc8t0?si=Efck8qAbZL-KYstU' },
+        { posicao: 5, classe: 'Druída', build: 'Wind Shear', nome: 'Junin', tier: 105, tempo: '12:28', video: 'https://youtu.be/LFjy_89r1uc?si=pkB15t1EBXu0Ew1f' },
 
 
     ];
