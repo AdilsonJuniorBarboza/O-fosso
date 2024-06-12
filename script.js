7717bca822a92cc3966b04435694d6f363ca3123
AdilsonJuniorBarboza <168775316+AdilsonJuniorBarboza@users.noreply.github.com>
2024-06-12 09:25:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     let rankingData = [
-        { classe: 'Bárbaro', build: 'Bash/Bleed', nome: 'Shelby', tier: 119, tempo: '7:54', modo: 'SC', video: 'https://www.youtube.com/watch?v=Gwu-mZ2iHqQ' },
+        { classe: 'Bárbaro', build: 'Bash/Bleed', nome: 'Shelby', tier: 123, tempo: '8:10', modo: 'SC', video: 'https://www.youtube.com/watch?v=A-cyRXJVhl4' },
         { classe: 'Bárbaro', build: 'Thorns(Espinhos)', nome: 'Tanure', tier: 118, tempo: '10:00', modo: 'SC', video: 'https://www.youtube.com/watch?v=zGLs7OGUdYA' },
         { classe: 'Rogue', build: 'Heartseeker', nome: 'Junin', tier: 113, tempo: '14:19', modo: 'SC', video: 'https://youtu.be/kTVMPcm7IV4' },
         { classe: 'Rogue', build: 'Heartseeker', nome: 'Veloso', tier: 110, tempo: '7:15', modo: 'SC', video: 'https://youtu.be/sAZwc2cc8t0?si=Efck8qAbZL-KYstU' },
         { classe: 'Druída', build: 'Wind Shear', nome: 'Junin', tier: 105, tempo: '12:28', modo: 'SC', video: 'https://youtu.be/LFjy_89r1uc?si=pkB15t1EBXu0Ew1f' },
         { classe: 'Necromante', build: 'Shadow Mage', nome: 'Offo_hc', tier: 79, tempo: '5:03', modo: 'HC', video: 'https://clips.twitch.tv/OptimisticKindHippoNotATK-N-8D1qH9x4N2rbo_' },
         { classe: 'Bárbaro', build: 'Bash', nome: 'Meganord', tier: 122, tempo: '10:08', modo: 'SC', video: 'https://youtu.be/W7omv2NMSVQ?si=g3aO8YjDGWMHs9fU' },
-        { classe: 'Rogue', build: 'Heartseeker', nome: 'Winester', tier: 70, tempo: '4:44', modo: 'HC', video: 'https://www.youtube.com/watch?v=DXBKPXSnM6k' },
+        { classe: 'Rogue', build: 'Heartseeker', nome: 'Winester', tier: 82, tempo: '5:25', modo: 'HC', video: 'https://www.youtube.com/watch?v=zwAQIIbpknI' },
         { classe: 'Bárbaro', build: 'Bash/Bleed', nome: 'Try', tier: 122, tempo: '11:15', modo: 'SC', video: 'https://youtu.be/r4bXK-DmDz8?si=hiAwWZwfoSA7oOWB' },
         { classe: 'Necromante', build: 'Bone spear minion', nome: 'Fleezao', tier: 112, tempo: '9:22', modo: 'SC', video: 'https://www.youtube.com/watch?v=jVecyhfYsro&t=22s&ab_channel=Fleezao' },
-
+        { classe: 'Rogue', build: 'Heatseeker', nome: 'Shineray', tier: 128, tempo: '14:24', modo: 'SC', video: 'https://www.youtube.com/watch?v=-TiyNQhV99g' },
+        { classe: 'Necromante', build: 'Shadow Golem', nome: 'ViTiN', tier: 130, tempo: '14:10', modo: 'SC', video: 'https://www.youtube.com/watch?v=FDiUrSIuQR8&ab_channel=VitorQueiroz' },
     ];
 
     rankingData.sort((a, b) => {
