ca81c97b286e8db0186e3cc78e36c04fb858bae2
AdilsonJuniorBarboza <168775316+AdilsonJuniorBarboza@users.noreply.github.com>
2024-06-13 12:57:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,17 +5,18 @@ document.addEventListener('DOMContentLoaded', () => {
         { classe: 'Rogue', build: 'Heartseeker', nome: 'Junin', tier: 113, tempo: '14:19', modo: 'SC', video: 'https://youtu.be/kTVMPcm7IV4' },
         { classe: 'Rogue', build: 'Heartseeker', nome: 'Veloso', tier: 110, tempo: '7:15', modo: 'SC', video: 'https://youtu.be/sAZwc2cc8t0?si=Efck8qAbZL-KYstU' },
         { classe: 'Druída', build: 'Wind Shear', nome: 'Junin', tier: 105, tempo: '12:28', modo: 'SC', video: 'https://youtu.be/LFjy_89r1uc?si=pkB15t1EBXu0Ew1f' },
-        { classe: 'Necromante', build: 'Shadow Mage', nome: 'Offo_hc', tier: 79, tempo: '5:03', modo: 'HC', video: 'https://clips.twitch.tv/OptimisticKindHippoNotATK-N-8D1qH9x4N2rbo_' },
+        { classe: 'Necromante', build: 'Shadow Golem', nome: 'Offo_hc', tier: 82, tempo: '5:03', modo: 'HC', video: 'https://clips.twitch.tv/MoldyFuriousAnacondaDatBoi-L3BzBiwpWDM58m3R' },
         { classe: 'Bárbaro', build: 'Bash', nome: 'Meganord', tier: 122, tempo: '10:08', modo: 'SC', video: 'https://youtu.be/W7omv2NMSVQ?si=g3aO8YjDGWMHs9fU' },
         { classe: 'Rogue', build: 'Heartseeker', nome: 'Winester', tier: 85, tempo: '4:51', modo: 'HC', video: 'https://www.youtube.com/watch?v=bafY1MbNIPg' },
         { classe: 'Bárbaro', build: 'Bash/Bleed', nome: 'Try', tier: 124, tempo: '13:07', modo: 'SC', video: 'https://youtu.be/8r1S5adJBXk?si=1b6kFAFDbCRpdCY-' },
         { classe: 'Necromante', build: 'Bone spear minion', nome: 'Fleezao', tier: 112, tempo: '9:22', modo: 'SC', video: 'https://www.youtube.com/watch?v=jVecyhfYsro&t=22s&ab_channel=Fleezao' },
         { classe: 'Rogue', build: 'Heartseeker', nome: 'Shineray', tier: 128, tempo: '14:24', modo: 'SC', video: 'https://www.youtube.com/watch?v=-TiyNQhV99g' },
         { classe: 'Necromante', build: 'Shadow Golem', nome: 'ViTiN', tier: 130, tempo: '14:10', modo: 'SC', video: 'https://www.youtube.com/watch?v=FDiUrSIuQR8&ab_channel=VitorQueiroz' },
-        { classe: 'Bárbaro', build: 'Bleed/HotA', nome: 'Gripp', tier: 131, tempo: '9:30', modo: 'SC', video: 'https://youtu.be/4Y7wOL8pYxI?si=LouMqu5nI1DbefHh' },
+        { classe: 'Bárbaro', build: 'Bash/Bleed/HotA', nome: 'Gripp', tier: 133, tempo: '14:30', modo: 'SC', video: 'https://youtu.be/eKFN9dv2xhI' },
         { classe: 'Rogue', build: 'Andariel Flurry', nome: 'Seonuk', tier: 110, tempo: '4:49', modo: 'SC', video: 'https://www.youtube.com/watch?v=hq848TPtRiQ' },
         { classe: 'Mago', build: 'Nevasca(Blizzard)', nome: 'Léo Quixeré', tier: 100, tempo: '9:32', modo: 'SC', video: 'https://www.youtube.com/watch?v=1yoMvTQz9iA&t=8s' },
         { classe: 'Mago', build: 'Immortal Frozen Orb', nome: 'Hisoka', tier: 113, tempo: '10:06', modo: 'SC', video: 'https://www.youtube.com/watch?v=k7nyM1uwOeQ' },
+        { classe: 'Necromante', build: 'Shadow Golem', nome: 'Hisoka', tier: 124, tempo: '7:52', modo: 'SC', video: 'https://www.youtube.com/watch?v=7J0EIrIwwyg&ab_channel=VilasBoas' },
     ];
 
     rankingData.sort((a, b) => {
