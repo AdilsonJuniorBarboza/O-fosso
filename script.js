7fc0b0f9c805561e37f81cbf7e78e6c8f7125d24
AdilsonJuniorBarboza <168775316+AdilsonJuniorBarboza@users.noreply.github.com>
2024-07-29 16:01:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,61 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     let rankingData = [
-        { classe: 'Bárbaro', build: 'Thorns(Espinhos)', nome: 'Tanure', tier: 118, tempo: '10:00', modo: 'SC', video: 'https://www.youtube.com/watch?v=zGLs7OGUdYA' },
-        { classe: 'Rogue', build: 'Heartseeker', nome: 'Junin', tier: 113, tempo: '14:19', modo: 'SC', video: 'https://youtu.be/kTVMPcm7IV4' },
-        { classe: 'Rogue', build: 'Heartseeker', nome: 'Veloso', tier: 110, tempo: '7:15', modo: 'SC', video: 'https://youtu.be/sAZwc2cc8t0?si=Efck8qAbZL-KYstU' },
-        { classe: 'Druída', build: 'Wind Shear Glass Cannon', nome: 'Junin', tier: 120, tempo: '13:37', modo: 'SC', video: 'https://youtu.be/S3mVfE_banU' },
-        { classe: 'Necromante', build: 'Shadow Golem', nome: 'Offo_hc', tier: 109, tempo: '06:11', modo: 'HC', video: 'https://clips.twitch.tv/HeadstrongGeniusPheasantPJSalt-_dLsYXrID0h4t9lS' },
-        { classe: 'Bárbaro', build: 'Bash', nome: 'Meganord', tier: 122, tempo: '10:08', modo: 'SC', video: 'https://youtu.be/W7omv2NMSVQ?si=g3aO8YjDGWMHs9fU' },
-        { classe: 'Rogue', build: 'Heartseeker', nome: 'Winester', tier: 112, tempo: '10:26', modo: 'HC', video: 'https://www.youtube.com/watch?v=kU5EMzA9v20' },
-        { classe: 'Bárbaro', build: 'Flay/Bleed', nome: 'Try', tier: 141, tempo: '11:09', modo: 'SC', video: 'https://youtu.be/Cs6NtCouJUs?si=WgtSYSlNuLlQ63w9' },
-        { classe: 'Necromante', build: 'Bone spear', nome: 'Fleezao', tier: 126, tempo: '13:54', modo: 'SC', video: 'https://www.youtube.com/watch?v=-M-YrNJoDJM&t=754s&ab_channel=Fleezao' },
-        { classe: 'Rogue', build: 'Heartseeker', nome: 'Shineray', tier: 137, tempo: '12:58', modo: 'SC', video: 'https://youtu.be/GgBNAKusGas' },
-        { classe: 'Necromante', build: 'Shadow Golem', nome: 'ViTiN', tier: 130, tempo: '14:10', modo: 'SC', video: 'https://www.youtube.com/watch?v=FDiUrSIuQR8&ab_channel=VitorQueiroz' },
-        { classe: 'Bárbaro', build: 'Bash/Bleed/HotA', nome: 'Gripp', tier: 133, tempo: '14:30', modo: 'SC', video: 'https://youtu.be/eKFN9dv2xhI' },
-        { classe: 'Rogue', build: 'Andariel Flurry', nome: 'Seonuk', tier: 135, tempo: '13:22', modo: 'SC', video: 'https://www.youtube.com/watch?v=X6WHgDuEKnc' },
-        { classe: 'Mago', build: 'Nevasca(Blizzard)', nome: 'Léo Quixeré', tier: 100, tempo: '9:32', modo: 'SC', video: 'https://www.youtube.com/watch?v=1yoMvTQz9iA&t=8s' },
-        { classe: 'Mago', build: 'Immortal Frozen Orb', nome: 'Hisoka', tier: 120, tempo: '10:14', modo: 'SC', video: 'https://youtu.be/UV0x3ytZRFA' },
-        { classe: 'Necromante', build: 'Shadow Golem', nome: 'VilasBoas', tier: 136, tempo: '14:05', modo: 'SC', video: 'https://youtu.be/XsJ2NrKO3VM' },
-        { classe: 'Mago', build: 'Firekuna Shatter (Versão com inferno)', nome: 'Shineray', tier: 133, tempo: '13:03', modo: 'SC', video: 'https://youtu.be/hMJGPUCxEM0' },
-        { classe: 'Rogue', build: 'Heartseeker', nome: 'JahIthBer', tier: 108, tempo: '14:20', modo: 'SC', video: 'https://youtu.be/kuRYPC5qCzw' },
-        { classe: 'Rogue', build: 'Heartseeker', nome: 'defreshXD', tier: 133, tempo: '13:47', modo: 'SC', video: 'https://www.youtube.com/watch?v=aV0qUzmZ8EQ' },
-        { classe: 'Rogue', build: 'Heartseeker', nome: 'DECO', tier: 100, tempo: '9:14', modo: 'SC', video: 'https://youtu.be/vLPwkz6D4sY?si=vFTlLvvioCD4uMdH' },
-        { classe: 'Bárbaro', build: 'Bleed Bash (Versão Própria)', nome: 'Bocca', tier: 144, tempo: '12:33', modo: 'SC', video: 'https://youtu.be/ojHs1UwsrN0' },
-        { classe: 'Bárbaro', build: 'Bash', nome: 'Winester', tier: 92, tempo: '6:20', modo: 'HC', video: 'https://www.youtube.com/watch?v=kpSU0GuKLtI' },
-        { classe: 'Necromante', build: 'Minions Golem', nome: 'gouggwww', tier: 129, tempo: '5:02', modo: 'SC', video: 'https://www.youtube.com/watch?v=jvBFIeUj0cs' },
-        { classe: 'Bárbaro', build: 'Flay/Rupture', nome: 'Shineray', tier: 116, tempo: '14:42', modo: 'SC', video: 'https://youtu.be/8smwKrSHelk' },
-        { classe: 'Necromante', build: 'Shadow Golem', nome: 'Lit', tier: 120, tempo: '12:10', modo: 'HC', video: 'https://www.youtube.com/watch?v=hyZXSjr8Xqg' },
-        { classe: 'Bárbaro', build: 'Espinhos (Sem Uber)', nome: 'BBzAUM', tier: 76, tempo: '4:09', modo: 'HC', video: 'https://youtu.be/IaYZgXVT-P8' }, 
-        { classe: 'Rogue', build: 'Heartseeker', nome: 'Yago', tier: 113, tempo: '9:33', modo: 'HC', video: 'https://www.youtube.com/watch?v=1qqQ7_hR-V4' },
-        { classe: 'Druída', build: 'Pulverize', nome: 'LUAN', tier: 80, tempo: '11:20', modo: 'SC', video: 'https://youtu.be/X0xsWH8Njq8?si=fyvNfIqBqkhMPorj' },
-        { classe: 'Necromante', build: 'Infinimist', nome: 'Lit', tier: 100, tempo: '06:17', modo: 'HC', video: 'https://www.youtube.com/watch?v=6AtaYGfQ_qo' },
-        { classe: 'Rogue', build: 'Heartseeker', nome: 'CASALE', tier: 131, tempo: '13:10', modo: 'SC', video: 'https://youtu.be/1D-2cwip4PI' },
-        { classe: 'Bárbaro', build: 'Bleed Bash / Tyrael ', nome: 'MTZ', tier: 128, tempo: '14:45', modo: 'SC', video: 'https://youtu.be/3xqohngwtzw?si=6pEBYOnKUzlKEVeC' },
-        { classe: 'Druída', build: 'PULVERIZE Envenom + Pets', nome: 'Junin', tier: 115, tempo: '14:19', modo: 'SC', video: 'https://youtu.be/eI2iyEe9Ak0?si=crbmyXRhIEbFlgjo' },
-        { classe: 'Mago', build: 'Fire Bolt / Inferno', nome: 'Immanewbie', tier: 153, tempo: '14:05', modo: 'SC', video: 'https://youtube.com/clip/Ugkx-yq1AHon-yQ5J3xZ_n4G6vNBUuD0OWB-?si=ex8753Nd8Ja93vtd' },
-        { classe: 'Bárbaro', build: 'WhirlWind', nome: 'Veloso', tier: 120, tempo: '5:54', modo: 'SC', video: 'https://youtu.be/SAvIgVhiHvE' },
-        { classe: 'Necromante', build: 'Blight', nome: 'Serjão', tier: 100, tempo: '9:16', modo: 'SC', video: 'https://youtu.be/z4kJOF76eaE?si=LgB5LnIp_LN-nTq9' },
-        { classe: 'Necromante', build: 'Bone Spirit', nome: 'Serjão', tier: 100, tempo: '10:12', modo: 'SC', video: 'https://youtu.be/TXd1keqp6Gg?si=RCY0PEMEkrzD594O' },
-        { classe: 'Druída', build: 'Lightning Storm (Grizzly Rage)', nome: 'Junin', tier: 100, tempo: '4:45', modo: 'SC', video: 'https://youtu.be/FbfRsKhNNwQ?si=EviyoGfaBEK8Qoio' },
-        { classe: 'Bárbaro', build: 'WW Bleed Tyrael', nome: 'DJLada', tier: 104, tempo: '4:32', modo: 'HC', video: 'https://youtu.be/yXndPcPhC6E' },
-        { classe: 'Mago', build: 'Firebolt', nome: 'pogg', tier: 126, tempo: '14:28', modo: 'SC', video: 'https://youtu.be/Qvm42-1IEYw' },
-        { classe: 'Bárbaro', build: 'Tyrael Bash Bleed', nome: 'xLordByKoNx', tier: 137, tempo: '14:20', modo: 'SC', video: 'https://www.youtube.com/watch?v=evwNCLCueRw' },
-        { classe: 'Bárbaro', build: 'Flay Rupture', nome: 'xixStres', tier: 143, tempo: '13:27', modo: 'SC', video: 'https://youtu.be/_DElfO66Fq8?si=bcqtuQWhzYh6TZPw' },
-        { classe: 'Rogue', build: 'Andariel Flurry', nome: 'Felipe', tier: 143, tempo: '14:04', modo: 'SC', video: 'https://youtu.be/B-zVLa0dYBs' },
-        { classe: 'Rogue', build: 'Pen Shot + Imbuing', nome: 'Serjão', tier: 100, tempo: '9:37', modo: 'SC', video: 'https://youtu.be/qoKm7RZZ0UA?si=UqpJr9Aj44Xp61pj' },
-        { classe: 'Necromante', build: 'Golem + Blood Mist', nome: 'BBzAUM', tier: 82, tempo: '5:59', modo: 'HC', video: 'https://youtu.be/FJFrwgVZkd4' },
-        { classe: 'Bárbaro', build: 'Whirlwind', nome: 'LUAN', tier: 103, tempo: '4:51', modo: 'SC', video: 'https://youtu.be/EERhBHTAA4k' }, 
-        { classe: 'Rogue', build: 'Heartseeker', nome: 'val4dao', tier: 142, tempo: '14:44', modo: 'SC', video: 'https://www.youtube.com/watch?v=MVroT71EzP8&ab_channel=vaL4dao' },
-        { classe: 'Bárbaro', build: 'Bleed Bash', nome: 'Keven', tier: 130, tempo: '13:57', modo: 'HC', video: 'https://www.youtube.com/watch?v=nvgl4xhyQtw' },
-        { classe: 'Rogue', build: 'Heartseeker', nome: 'Arwen', tier: 136, tempo: '14:54', modo: 'SC', video: 'https://youtu.be/ayfJv3XU8DE' },
-        { classe: 'Bárbaro', build: 'Bleed Bash', nome: 'Zaibaonehit', tier: 138, tempo: '15:00', modo: 'SC', video: 'https://youtu.be/ILAun6Je8ac' },
-        { classe: 'Bárbaro', build: 'Whirlwind', nome: 'ElfoGUERRA', tier: 108, tempo: '14:38', modo: 'SC', video: 'https://youtu.be/KJY3W1XXdps' },
-        { classe: 'Druída', build: 'Andys Claw', nome: 'WildBlackSmith', tier: 107, tempo: '11:46', modo: 'SC', video: 'https://www.twitch.tv/videos/2188294912' },
-        { classe: 'Druída', build: 'Maul', nome: 'WildBlackSmith', tier: 90, tempo: '11:07', modo: 'SC', video: 'https://www.twitch.tv/videos/2187726633' },
-        { classe: 'Necromante', build: 'Shadow Mage', nome: 'Fleezao', tier: 137, tempo: '14:14', modo: 'SC', video: 'https://www.youtube.com/watch?v=pxTee0Ppx4k&ab_channel=Fleezao' },
-        { classe: 'Rogue', build: 'Andariel Flurry', nome: 'Veloso', tier: 131, tempo: '11:06', modo: 'SC', video: 'https://youtu.be/CXsX2vb-Xk4' },
-        { classe: 'Necromante', build: 'Minions', nome: 'Veloso', tier: 80, tempo: '3:37', modo: 'HC', video: 'https://youtu.be/CoUbVNCUC84' },
-        { classe: 'Mago', build: 'InfernoKuna ', nome: 'Léo Quixeré', tier: 140, tempo: '15:00', modo: 'SC', video: 'https://www.youtube.com/watch?v=WPGgmVc2vHM' },
+        { classe: '', build: '', nome: '', tier: , tempo: '', modo: '', video: '' },
+
     ];
 // { classe: '', build: '', nome: '', tier: 0, tempo: '', modo: 'SC', video: '' },
     rankingData.sort((a, b) => {
@@ -110,12 +56,12 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
-// Função para rolar suavemente até o elemento especificado
+
 function searchAndScroll() {
     const searchInput = document.getElementById('searchInput').value.trim().toLowerCase();
     const rows = document.querySelectorAll('#ranking-body tr');
 
-    // Verifica se o campo de pesquisa está vazio
+ 
     if (searchInput === '') {
         alert('Digite um nome para pesquisar.');
         return;
@@ -126,40 +72,45 @@ function searchAndScroll() {
     rows.forEach(row => {
         const nomeCell = row.querySelector('td:nth-child(4)');
         const nome = nomeCell.textContent.trim().toLowerCase();
-        if (nome.includes(searchInput)) { // Verifica se o nome contém o texto pesquisado
-            // Scroll para o elemento encontrado com animação
+        if (nome.includes(searchInput)) { 
+
             row.scrollIntoView({ behavior: 'smooth', block: 'center' });
 
-            // Destaque somente o texto encontrado em amarelo
+
             const texto = nomeCell.innerHTML;
             const textoDestacado = texto.replace(new RegExp(`(${searchInput})`, 'gi'), '<span style="background-color: yellow">$1</span>');
             nomeCell.innerHTML = textoDestacado;
 
-            // Remover o destaque após alguns segundos (opcional)
+
             setTimeout(() => {
-                nomeCell.innerHTML = texto; // Restaura o texto original
-            }, 3000); // 3000 milissegundos = 3 segundos
+                nomeCell.innerHTML = texto;
+            }, 3000); 
 
             found = true;
         }
     });
 
-    // Se nenhum nome correspondente foi encontrado
+
     if (!found) {
         alert('Nome não encontrado na tabela.');
     }
 }
 
-// Seleciona o elemento de input de pesquisa
+
 const searchInput = document.getElementById('searchInput');
 
-// Adiciona um event listener para o evento 'keydown' no input
+
 searchInput.addEventListener('keydown', function(event) {
-    // Verifica se a tecla pressionada é 'Enter'
+
     if (event.key === 'Enter') {
-        // Chama a função de pesquisa ao pressionar 'Enter'
+
         searchAndScroll();
     }
+
+    window.onload = function() {
+        populateTable();
+        updateSeasonSelect();
+    };
 });
 
 
