0b8839806cf0d707554f5fa7e44f27fa121dc913
AdilsonJuniorBarboza <168775316+AdilsonJuniorBarboza@users.noreply.github.com>
2024-08-26 15:01:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     let rankingData = [
         { classe: 'Druída', build: 'Storm Slide', nome: 'Junin', tier: 110, tempo: '6:27', modo: 'SC', video: 'https://youtu.be/yAfxU2OhCCo' },
-
+        { classe: 'Rogue', build: 'Rapid Fire', nome: 'Try', tier: 101, tempo: '10:42', modo: 'SC', video: 'https://youtu.be/n976FxfIj3w?si=gG0a90MB-bdD9FE5' },
+        { classe: 'Mago', build: 'Lightning Spear', nome: 'их', tier: 104, tempo: '1:59', modo: 'SC', video: 'https://youtu.be/cXeNZ_dfgWY' },
+        { classe: 'Mago', build: 'Laggy Lightning Spear', nome: 'Immanewbie', tier: 135, tempo: '11:45', modo: 'SC', video: 'https://youtube.com/clip/UgkxAknTyviqyYJylVojKtTHt5DgP-ZmPo6u?si=UfJfc3tXk5IGWEn7' },
+        { classe: 'Druída', build: 'Doomstorm', nome: 'VasenasGamer', tier: 121, tempo: '7:10', modo: 'SC', video: 'https://www.tiktok.com/@vasenasgamer/video/7405645228574559494?is_from_webapp=1&sender_device=pc&web_id=7395274450344658438' },
     ];
 // { classe: '', build: '', nome: '', tier: 0, tempo: '', modo: 'SC', video: '' },
     rankingData.sort((a, b) => {
